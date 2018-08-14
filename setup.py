0485e901438114b2797fd8a2ddb6c9970be68b70
Mikhail I. Izmestev <izmmishao5@gmail.com>
2018-08-14 09:46:02 +0400

    Add msbuild integration tests
    * test incremental build with VS2013
    * test incremental build with VS2015
    * test clean target with VS2015 causing clcache files removed

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
     entry_points={
           'console_scripts': [
               'clcache = clcache.__main__:main',
+              'cl.cache = clcache.__main__:main',
               'clcache-server = clcache.server.__main__:main',
           ]
     },
---
+++ tests/integrationtests/msbuild/another.cpp
@@ -0,0 +1,1 @@
+int somefunc() { return 1; }
